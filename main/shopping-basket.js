a95f7b34e7f4e52df739f349dfbd574a452b6e44
anlin-yang <anlin0414@outlook.com>
2015-09-08 12:41:15 +0800

    add function addItem() to shopping-basket.js file.

--- main/shopping-basket.js
+++ main/shopping-basket.js
@@ -2,4 +2,19 @@ function ShoppingBasket() {
   this.basketItems = [];
 }
 
+ShoppingBasket.prototype.addItem = function(title, count) {
+  var existItems = this.basketItems.filter(function(val) {
+    return val.title === title;
+  });
+
+  if (existItems.length !== 0) {
+    existItems[0].count += count;
+  } else {
+    this.basketItems.push({
+      title: title,
+      count: count
+    });
+  }
+}
+
 module.exports = ShoppingBasket;
