7bcfbc9a613bf0fc66c5ae696987c6dd9fe73fe4
anlin-yang <anlin0414@outlook.com>
2015-09-08 21:18:23 +0800

    add test file basket-item-spec.js to test BasketItem object.

---
+++ spec/main/basket-item-spec.js
@@ -0,0 +1,32 @@
+var BasketItem = require('../../main/basket-item.js');
+var Book = require('../../main/book.js');
+
+describe("BasketItem", function() {
+  var theBasketItem;
+
+  beforeEach(function() {
+    var bookArr = [
+      new Book('1st'),
+      new Book('2nd'),
+      new Book('3rd'),
+      new Book('4th'),
+      new Book('5th'),
+    ];
+    BasketItem.setAllBooks(bookArr);
+    theBasketItem = new BasketItem('1st', 2);
+  });
+
+  describe("constructor", function() {
+    it("should accept title, conut as parameters to create Object.", function() {
+      expect(theBasketItem.title).toBe('1st');
+      expect(theBasketItem.count).toBe(2);
+    });
+  });
+
+  describe("getPrice", function() {
+    it("should get correct Book price.", function() {
+      expect(theBasketItem.getPrice()).toBe(8);
+    });
+  });
+
+});
