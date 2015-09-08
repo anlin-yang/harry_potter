833fbf3c5e92c92b8b50b43239d39bc3de7f9106
小羊咩咩 <xiaoyangmiemie@xiaoyangmiemiedeMacBook.local>
2015-09-08 12:32:39 +0800

    add shopping-basket-spec content for test Object function addItem().

--- spec/main/shopping-basket-spec.js
+++ spec/main/shopping-basket-spec.js
@@ -4,6 +4,11 @@ var theShoppingBasket;
 
 beforeEach(function() {
   theShoppingBasket = new ShoppingBasket();
+  theShoppingBasket.addItem('1st', 2);
+  theShoppingBasket.addItem('2nd', 2);
+  theShoppingBasket.addItem('3rd', 2);
+  theShoppingBasket.addItem('4th', 1);
+  theShoppingBasket.addItem('5th', 1);
 });
 
 describe("ShoppingBasket", function() {
@@ -12,4 +17,17 @@ describe("ShoppingBasket", function() {
       expect(theShoppingBasket.basketItems instanceof Array).toBe(true);
     });
   });
+
+  describe("addItem", function() {
+    it("should correct add Book to the shoppingBasket.", function() {
+      expect(theShoppingBasket.basketItems.length).toBe(5);
+    });
+
+    it("shouldn`t add new elements，should be added to the existing.", function() {
+      theShoppingBasket.addItem('2nd', 3);
+      expect(theShoppingBasket.basketItems.length).toBe(5);
+    });
+  });
+
+
 });
