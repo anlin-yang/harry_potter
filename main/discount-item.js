eafb5a1d5f760e81bf3b5ca638203f0e0eda7b1d
anlin-yang <anlin0414@outlook.com>
2015-09-10 10:42:47 +0800

    add function getTotalPrice() from file discount-item.js.

--- main/discount-item.js
+++ main/discount-item.js
@@ -3,4 +3,14 @@ function DiscountItem(discountStrategy) {
   this.discItems = [];
 }
 
+DiscountItem.prototype.getTotalPrice = function() {
+  var totalPrice = 0;
+
+  this.discItems.forEach(function(val) {
+    totalPrice += 8;
+  });
+
+  return totalPrice;
+}
+
 module.exports = DiscountItem;
