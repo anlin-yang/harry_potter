5545af51e2ae68f86f23e22f330a6361bf638017
anlin-yang <anlin0414@outlook.com>
2015-09-10 13:01:01 +0800

    use injection method to add data for test.

--- spec/main/shopping-basket-spec.js
+++ spec/main/shopping-basket-spec.js
@@ -1,17 +1,27 @@
 var ShoppingBasket = require('../../main/shopping-basket.js');
-
-var theShoppingBasket;
-
-beforeEach(function() {
-  theShoppingBasket = new ShoppingBasket();
-  theShoppingBasket.addItem('1st', 2);
-  theShoppingBasket.addItem('2nd', 2);
-  theShoppingBasket.addItem('3rd', 2);
-  theShoppingBasket.addItem('4th', 1);
-  theShoppingBasket.addItem('5th', 1);
-});
+var DiscountStrategy = require('../../main/discount-strategy.js');
 
 describe("ShoppingBasket", function() {
+  var theShoppingBasket;
+
+  beforeEach(function() {
+    theShoppingBasket = new ShoppingBasket();
+    theShoppingBasket.addItem('1st', 2);
+    theShoppingBasket.addItem('2nd', 2);
+    theShoppingBasket.addItem('3rd', 2);
+    theShoppingBasket.addItem('4th', 1);
+    theShoppingBasket.addItem('5th', 1);
+
+    var discStrategys = [
+      new DiscountStrategy("BUY_ONE_BOOK_GET_0%_DISCOUNT."),
+      new DiscountStrategy("BUY_TWO_DIFFERENT_BOOKS_GET_5%_DISCOUNT.", 2, 0.05),
+      new DiscountStrategy("BUY_THREE_DIFFERENT_BOOKS_GET_10%_DISCOUNT.", 3, 0.1),
+      new DiscountStrategy("BUY_FOUR_DIFFERENT_BOOKS_GET_20%_DISCOUNT.", 4, 0.2),
+      new DiscountStrategy("BUY_FIVE_DIFFERENT_BOOKS_GET_25%_DISCOUNT.", 5, 0.25)
+    ];
+    ShoppingBasket.setDiscStrategys(discStrategys);
+  });
+
   describe("constructor", function() {
     it("basketItems`s type should return Array.", function() {
       expect(theShoppingBasket.basketItems instanceof Array).toBe(true);
