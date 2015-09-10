779265746f808b964c43b64f67c1a69b72f248ce
anlin-yang <anlin0414@outlook.com>
2015-09-10 11:57:52 +0800

    add function getTotalPrice() from grouping-way.js to calculate the total price.

--- main/grouping-way.js
+++ main/grouping-way.js
@@ -2,4 +2,13 @@ function GroupingWay() {
   this.groupingItems = [];
 }
 
+GroupingWay.prototype.getTotalPrice = function() {
+  var totalPrice = 0;
+  this.groupingItems.forEach(function(val) {
+    totalPrice += val.getTotalPrice();
+  });
+
+  return totalPrice;
+};
+
 module.exports = GroupingWay;
