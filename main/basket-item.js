7ce5e7527e8607b9d579a9375161da43d0d89d01
anlin-yang <anlin0414@outlook.com>
2015-09-10 09:55:02 +0800

    modified basket-item.js for add attribute book and delete cache function. pass the test.

--- main/basket-item.js
+++ main/basket-item.js
@@ -1,33 +1,18 @@
+var Book = require('./book.js');
 var allBooks = [];
 
 function BasketItem(title, count) {
   this.title = title;
   this.count = count;
+  this.book = new Book(title);
 };
 
 BasketItem.setAllBooks = function(bookArr) {
   allBooks = bookArr;
 };
 
-BasketItem.prototype._getBook = function() {
-  if (this._item) {
-    return this._item;
-  } else {
-    var that = this;
-    var book;
-    allBooks.forEach(function(val) {
-      if (val.title === that.title) {
-        book = val;
-      }
-    });
-    this._item = book;
-    return book;
-  }
-};
-
 BasketItem.prototype.getPrice = function() {
-  var book = this._getBook();
-  return book.price;
+  return this.book.price;
 };
 
 module.exports = BasketItem;
