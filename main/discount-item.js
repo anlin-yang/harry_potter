e937f566f74184e0d31e656f35c98283dae1ae13
anlin-yang <anlin0414@outlook.com>
2015-09-10 10:52:41 +0800

    add function getSubtotalPrice() from discount-item.js to get the price after discount.

--- main/discount-item.js
+++ main/discount-item.js
@@ -5,12 +5,21 @@ function DiscountItem(discountStrategy) {
 
 DiscountItem.prototype.getTotalPrice = function() {
   var totalPrice = 0;
-
   this.discItems.forEach(function(val) {
     totalPrice += 8;
   });
 
   return totalPrice;
-}
+};
+
+DiscountItem.prototype.getSubtotalPrice = function() {
+  var discountPrice = 0;
+  var that = this;
+  this.discItems.forEach(function(val) {
+    discountPrice += 8 * that.discountStrategy.discount;
+  });
+
+  return this.getTotalPrice() - discountPrice;
+};
 
 module.exports = DiscountItem;
